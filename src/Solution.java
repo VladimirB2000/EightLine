98a1881e9a90cef2404c1094f96c58748932c6a9
Vladimir Belkin <vladimirB2000@mail.ru>
2019-08-06 20:35:42 +0700

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        for(int i =1; i<10; i++)
+            System.out.print("8");
+        for(int d =1; d<12; d++)
+            System.out.println("8");
+    }
+}
